adee356f02d9a19e0ed58f2fb0ce21ff09d118aa
Zachary Fraedrich <zfraedri@asu.edu>
2025-02-04 14:34:06 -0700

    updated JS
    edited and finished function practice in class

--- scripts.js
+++ scripts.js
@@ -1,44 +1,63 @@
 // Functions Practice
 // For the first standard function, write a function that accepts two numbers as parameters, adds them together, then returns the result of that addition
 // name this function addTwoNums
-// TO DO
+
+function addTwoNums(num1, num2){
+    return num1 + num2;
+}
+
+
 
 
 // For box 2, let's accept two string parameters and return the result of them concatenated into a single string with a space between the two words
 // name this function concatenateStrings
-// TO DO
+
+function concatenateStrings(string1,string2){
+    return string1 + " " + string2;
+}
+
 
 
 // for the first function expression, let's write code that displays returns one result if the number entered by the user is less than 100 ("number is less than 100"), and another message if the number is greater than or equal to 100 ("that is 100 or greater")
 // name this variable compareToHundred
-// TO DO
+
+let compareToHundred = function(number){
+    return number < 100 ? "number is less than 100" : "that is 100 or greater";
+};
+
+
 
 
 // for the second function expression, let's write code that tells the user whether or not their answer to a trivia question is correct. This will accept a parameter for a letter answer, and can use either a switch statement or a series of if/else/if statements for the logic
 // name this variable quizAnswer
-// TO DO
-		// if they answered a return "Nope. GIT215 is the Introductory HTML/CSS course."
 
-	
-		// if they answered b return "Nope. GIT337 is the Intermediate HTML/CSS course."
+let quizAnswer = function(answer){
+    switch(answer){
+        case "a":
+            return "Nope. GIT215 is the Introductory HTML/CSS course."
+        case "b":
+            return "Nope. GIT337 is the Intermediate HTML/CSS course."
+        case "c":
+            return "Nope. GIT414 is the Responsive HTML/CSS course."
+        case "d":
+            return "You got it! GIT417 is the Introductory JavaScript course."
+    }
+}
 
-	
-		// if they answered c return "Nope. GIT414 is the Responsive HTML/CSS course."
 
-	
-		// if they answered d return "You got it! GIT417 is the Introductory JavaScript course."
 
 
 // For box five, re-write the first standard function in the arrow function syntax
 // name the variable addTwoNumsArrow
-// TO DO
+
+let addTwoNumsArrow =(a,b) => a + b
 
 
 // For box five, re-write the second standard function in the arrow function syntax
 // name the variable concatenateStringsArrow
 // TO DO
 
-
+let concatenateStringsArrow = (part1,part2) => part1 + " " + part2
 
 
 
